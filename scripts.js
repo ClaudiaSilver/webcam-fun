c1e39f51d3048bbb24f79720dc9576c25822cde3
Claudia Silver <claudiasilver@Claudias-MBP.home>
2019-09-16 20:14:45 +0100

    add functions

--- scripts.js
+++ scripts.js
@@ -27,6 +27,14 @@ function paintToCanvas() {
 
     setInterval(() => {
         ctx.drawImage(video, 0, 0, width, height);
+        // take the pixels out
+        let pixels = ctx.getImageData(0,0, width, height);
+        // mess with them
+        //pixels = redEffect(pixels);
+        pixels = rgbSplit(pixels);
+        ctx.globalAlpha = 0.1; // sets the alpha(i.e. transparency value)
+        // put them back
+        ctx.putImageData(pixels, 0, 0);
     }, 16); // every 16 milliseconds
 }
 
@@ -35,10 +43,39 @@ function takePhoto() {
     snap.currentTime = 0;
     snap.play();
 
-    // take the data out of the canvas
-    const data = canvas.toDataURL('image/jpeg');
+    // take the data out of the canvas and create download link
+    const data = canvas.toDataURL('image/jpeg'); // returns base 64 text-based representation of the picture
+    const link = document.createElement('a'); // backticks don't work here
+    link.href = data;
+    link.setAttribute('download', 'handsome');
+    link.innerHTML = `<img src ="${data}" alt="Fartface" />`
+    link.textContent = 'Download Image';
+    strip.insertBefore(link, strip.firstChild);
     
 }
+
+function redEffect(pixels) {
+    // loop over every single pixel:
+    for(let i = 0; i < pixels.data.length; i += 4) { //increment by 4 because of the 4 RGBA values which repeat in groups of 4
+        pixels.data[i] = pixels.data[i] + 200; // red channel
+        pixels.data[i + 1] = pixels.data[i + 1] - 50; // green channel
+        pixels.data[i + 2] =  pixels.data[i + 2] * 0.5; // blue channel
+    }
+    return pixels;
+}
+// this effect pulls apart the reds, greens and blues of the image
+function rgbSplit(pixels) {
+    for(let i = 0; i < pixels.data.length; i += 4) { //increment by 4 because of the 4 RGBA values which repeat in groups of 4
+        pixels.data[i - 150] = pixels.data[i]; // red channel
+        pixels.data[i + 100] = pixels.data[i + 1]; // green channel
+        pixels.data[i - 150] =  pixels.data[i + 2]; // blue channel
+}
+return pixels;
+
+function greenScreen(pixels) {
+    
+}
+
 
 getVideo(); // runs on page load
 
